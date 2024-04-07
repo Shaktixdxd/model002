77b92713248167eb774ccc3136b04af30b7136a7
Shaktixdxd <142348173+Shaktixdxd@users.noreply.github.com>
2024-04-08 02:12:37 +0530

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,10 +1,3 @@
-import pickle
-import streamlit as st
-
-#svm_model = pickle.load(open('final_svm.sav', 'rb'))
-#nb_model = pickle.load(open('final_nb.sav', 'rb'))
-#rf_model = pickle.load(open('final_rf.sav', 'rb'))
-
 import streamlit as st
 
 # Set page title
@@ -15,32 +8,21 @@ diseases = [
     "Disease 1", "Disease 2", "Disease 3",  # Add all 132 diseases here
 ]
 
-# Create dictionary to store checkbox values
-checkbox_values = {}
-
-# Display disease checkboxes
+# Display disease checkboxes and buttons
 for disease in diseases:
-    st.write(disease)
-    checkbox_values[disease] = st.number_input(f"Enter 0 or 1 for {disease}", min_value=0, max_value=1, value=0, step=1, key=disease)
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        st.write(disease)
+    with col2:
+        checkbox_value = st.number_input("", min_value=0, max_value=1, value=0, step=1)
 
 # Save button
 if st.button("Save"):
-    # Filter diseases with checkbox value of 1
-    selected_diseases = [disease for disease, value in checkbox_values.items() if value == 1]
-    # Sort selected diseases alphabetically
-    selected_diseases.sort()
-    # Display selected diseases as string in alphabetical order
-    selected_diseases_str = ", ".join(selected_diseases)
-    st.text_area("Selected Diseases:", value=selected_diseases_str, height=100)
+    selected_diseases = [disease for disease, value in zip(diseases, checkbox_values) if value == 1]
+    selected_diseases.sort()  # Sort selected diseases alphabetically
+    output_string = ", ".join(selected_diseases)
+    st.text_area("Selected Diseases:", value=output_string)
 
-    # Save selected diseases as string in code
-    selected_diseases_code = f'selected_diseases = "{selected_diseases_str}"'
-    st.text_area("Selected Diseases Code:", value=selected_diseases_code, height=100)
-
-
- # getting the input data from the user
-    col1 = st.columns(1)
-    
-    with col1:
-        Diseases = st.text_input('Number of Pregnancies')
-        
+# Copy button
+if st.button("Copy"):
+    st.text_area("Copied Text:", value=output_string, height=100)
