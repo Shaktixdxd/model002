c36e2fb34bb7f275a98484834d76318823468887
Shaktixdxd <142348173+Shaktixdxd@users.noreply.github.com>
2024-04-10 02:24:03 +0530

    Update example1.py

--- example1.py
+++ example1.py
@@ -29,8 +29,8 @@ data["prognosis"] = encoder.fit_transform(data["prognosis"])
 
 # Training and testing SVM Classifier
 svm_model = SVC()
-svm_model.fit(X_train, y_train)
-preds = svm_model.predict(X_test)
+#svm_model.fit(X_train, y_train)
+#preds = svm_model.predict(X_test)
 
 print(f"Accuracy on train data by SVM Classifier: {accuracy_score(y_train, svm_model.predict(X_train))*100}")
 print(f"Accuracy on test data by SVM Classifier: {accuracy_score(y_test, preds)*100}")
